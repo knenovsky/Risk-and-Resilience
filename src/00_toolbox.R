dfe5c536266efec0c68e661a1dd18b00bc6ca329
knenovsky <kolja.nenoff@posteo.de>
2023-07-28 15:29:25 +0200

    We add the variables

--- src/00_toolbox.R
+++ src/00_toolbox.R
@@ -57,3 +57,86 @@ plot_WDI_SD<-function(data=corrWDItest,Wdi_list=comparethiswdi,criterium_name,cr
   }
   return(WDI_plots)
 }
+
+
+train_xgboost_model <- function(testdata, Label, features, custom_control, param_grid) {
+  # Filter the data based on IsoYear and select relevant columns
+  thisData_2_input <- testdata %>% select(Label, features) %>% drop_na()
+  
+  
+  # Test different parameters
+  xgb_caret <- caret::train(
+    x = thisData_2_input %>% dplyr::select(features),
+    y = thisData_2_input %>% pull(Label),
+    method = "xgbTree",
+    verbosity = 0,
+    trControl = custom_control,tuneGrid=param_grid,
+    tuneLength = 10
+  )
+  
+  finalmodel <- caret::train(
+    x = thisData_2_input %>% dplyr::select(features),
+    y = thisData_2_input %>% pull(Label),
+    method = "xgbTree",
+    verbosity = 0,
+    trControl = trainControl(method = "cv", number = 30),
+    tuneGrid = xgb_caret$bestTune
+  )
+  
+  return(finalmodel)
+}
+calculate_waves <- function(data) {
+  wave_list <- list()
+  wave_start <- NA
+  wave_end <- NA
+  found_max <- FALSE
+  global_minima <- c()
+  
+  for (i in 3:(nrow(data) - 2)) { # Skip first and last entries
+    # Calculate the slope to the next month and to the previous month
+    slope_next <- data$p_value_mort_smoothed[i+1] - data$p_value_mort_smoothed[i]
+    slope_prev <- data$p_value_mort_smoothed[i] - data$p_value_mort_smoothed[i-1]
+    
+    # Check for start of wave (local minima)
+    if (is.na(wave_start) && slope_prev < 0 && slope_next > 0) {
+      wave_start <- i
+    }
+    
+    # Check for maxima within wave
+    if (!is.na(wave_start) && !found_max && slope_prev > 0 && slope_next < 0) {
+      found_max <- TRUE
+    }
+    
+    # Check for end of wave (local minima) and avoid capturing minima within 3 months of the start
+    if (!is.na(wave_start) && found_max && slope_prev < 0 && slope_next > 0 && (i - wave_start) > 3) {
+      wave_end <- i
+      global_minima <- c(global_minima, i)  # Save global minima
+    }
+    
+    # If wave ended, add it to the list
+    if (!is.na(wave_start) && !is.na(wave_end)) {
+      wave_list <- c(wave_list, list(data[wave_start:wave_end, ]))
+      wave_start <- NA
+      wave_end <- NA
+      found_max <- FALSE
+    }
+  }
+  
+  # Check for open wave at end of data
+  if (!is.na(wave_start) && found_max) {
+    wave_list <- c(wave_list, list(data[wave_start:(nrow(data)-1), ])) # Adjust for NA at the end
+  }
+  
+  # If only one wave is found and it starts after 9 months from the beginning of the data
+  if(length(wave_list) == 1 && wave_list[[1]][1, ]$date >= as.Date("2020-09-01")) {
+    first_global_minima <- wave_list[[1]][1, ]$date
+  } else if (length(global_minima) >= 1) {
+    # If at least one global minima found, return the first one
+    first_global_minima <- data[global_minima[1],"date"]
+  } else {
+    first_global_minima <- NA
+  }
+  
+  return(list('waves' = wave_list, 'first_global_minima' = first_global_minima))
+}
+
